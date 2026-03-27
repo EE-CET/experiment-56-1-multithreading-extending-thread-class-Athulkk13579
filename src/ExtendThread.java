17db76870bb6af0cd5d216206cea1262d37928f3
Athulkk13579 <athulkrishnak8086@gmail.com>
2026-03-27 15:58:56 +0530

    Update ExtendThread.java

--- src/ExtendThread.java
+++ src/ExtendThread.java
@@ -1,13 +1,13 @@
+
 class MyThread extends Thread {
-    @Override
     public void run() {
         System.out.println("Thread is running");
     }
 }
 
-public class ThreadDemo {
+public class ExtendThread {
     public static void main(String[] args) {
-        MyThread thread = new MyThread();
-        thread.start();
+        MyThread t = new MyThread();
+        t.start();
     }
 }
